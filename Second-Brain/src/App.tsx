9e374c2589194ff27bf0c75a800438edaf587082
unknown <mohdarish8482@gmail.com>
2025-05-10 10:40:58 +0530

    initialize FE for second-brain

---
+++ Second-Brain/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/Button";
+
+function App() {
+  return (
+    <>
+      <Button
+        varient={"primary"}
+        size={"md"}
+        text={"share"}
+        onClick={() => {}}
+      ></Button>
+    </>
+  );
+}
+
+export default App;
