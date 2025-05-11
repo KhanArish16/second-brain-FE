277d01fa9ef5267d657a370400e180765eff90ba
unknown <mohdarish8482@gmail.com>
2025-05-11 18:22:39 +0530

    created side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import Card from "./components/Card";
 import CreateContentModal from "./components/CreateContentModal";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
-      <CreateContentModal
-        open={modalOpen}
-        onClose={() => setModalOpen(false)}
-      />
-      <div className="p-4 bg-white flex gap-4">
-        <Card
-          title={"first Card"}
-          link={"https://www.youtube.com/watch?v=QoMHwks6hUA"}
-          type="youtube"
-        />
+      <Sidebar />
 
-        <Card
-          type="twitter"
-          title={"Second Card"}
-          link={"https://x.com/toptweets/status/1241821178643329024"}
+      <div className="p-4 ml-72 bg-gray-50 min-h-screen">
+        <CreateContentModal
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
         />
+        <div className="p-4  flex gap-4">
+          <Card
+            title={"first Card"}
+            link={"https://www.youtube.com/watch?v=QoMHwks6hUA"}
+            type="youtube"
+          />
+
+          <Card
+            type="twitter"
+            title={"Second Card"}
+            link={"https://x.com/toptweets/status/1241821178643329024"}
+          />
+        </div>
       </div>
     </>
   );
