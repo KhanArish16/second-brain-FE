a90095ea527382f90809973ced862d45b19f10eb
unknown <mohdarish8482@gmail.com>
2025-05-11 08:05:20 +0530

    start building FE

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,20 @@ function App() {
         variant={"primary"}
         size="lg"
         title={"Share"}
+        startIcon={<PlusIcon size={"lg"} />}
+      ></Button>
+      <Button
+        variant={"secondary"}
+        size="md"
+        title={"Share"}
         startIcon={<PlusIcon size={"md"} />}
       ></Button>
-
-      <Button variant={"secondary"} size="md" title={"Share"}></Button>
+      <Button
+        variant={"primary"}
+        size="sm"
+        title={"Share"}
+        startIcon={<PlusIcon size={"sm"} />}
+      ></Button>
     </>
   );
 }
